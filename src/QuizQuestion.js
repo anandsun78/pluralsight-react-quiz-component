b44912d7076a3e400aacaad2a52565c55cb570cc
anandsun78 <anandsun78@gmail.com>
2020-11-13 23:33:14 -0700

    Initial Commit

---
+++ src/QuizQuestion.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
+
+class QuizQuestion extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            incorrectAnswer: false
+        };
+    }
+    handleClick(buttonText){
+        if(buttonText === this.props.quiz_question.answer){
+            this.props.showNextQuestionHandler();
+            this.setState(()=>{
+                return {incorrectAnswer : false}})
+        }
+        else {
+            this.setState(()=>{
+                return {incorrectAnswer : true}})
+        }
+    }
+    render(){
+        return(
+        <main>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
+        <section>
+        <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+        <ul>  
+          {this.props.quiz_question.answer_options.map((answer_option, index)=>{
+            return(  
+            <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
+            );
+          })}          
+        </ul>
+        </section>
+      </main>
+        );
+    }
+}
+
+export default QuizQuestion;
